17d5475a5ad2e0ab4353731195c1c12e0eb05c54
tulasiram <ram.damarla@gmail.com>
2023-04-13 16:25:14 +0530

    reading and processing files

---
+++ src/chapter7/cipher.py
@@ -0,0 +1,87 @@
+##
+# This program encrypts a file using the Caesar cipher.
+# For ex the below command encrypts the file input.txt and places the result into encrypt.txt
+# python cipher.py input.txt encrypt.txt
+# The below command decrypts(-d) the file encrypt.txt and places the result into output.txt.
+# python cipher.py -d encrypt.txt output.txt
+#
+
+from sys import argv
+
+DEFAULT_KEY = 3
+
+
+def main():
+    key = DEFAULT_KEY
+    inFile = ""
+    outFile = ""
+    files = 0  # Number of command line arguments that are files.
+
+    for i in range(1, len(argv)):
+        arg = argv[i]
+        if arg[0] == "-":
+            # It is a command line option.
+            option = arg[1]
+            if option == "d":
+                key = -key
+            else:
+                usage()
+                return
+
+        else:
+            # It is a file name.
+            files = files + 1
+            if files == 1:
+                inFile = arg
+            elif files == 2:
+                outFile = arg
+
+    # There must be two files
+    if files != 2:
+        usage()
+        return
+
+    # Open the files.
+    inputFile = open(inFile, "r")
+    outputFile = open(outFile, "w")
+
+    # Read the characters from the file.
+    for line in inputFile:
+        for char in line:
+            newChar = encrypt(char, key)
+            outputFile.write(newChar)
+
+    # Close the files.
+    inputFile.close()
+    outputFile.close()
+
+
+## Encrypts upper- and lowercase characters by shifting them according to a key.
+# @param ch the letter to be encrypted
+# @param key the encryption key
+# @return the encrypted letter
+#
+def encrypt(ch, key):
+    LETTERS = 26  # Number of letters in the Roman alphabet.
+    if "A" <= ch <= "Z":
+        base = ord("A")
+    elif "a" <= ch <= "z":
+        base = ord("a")
+    else:
+        return ch  # Not a letter.
+    offset = ord(ch) - base + key
+    if offset > LETTERS:
+        offset = offset - LETTERS
+    elif offset < 0:
+        offset = offset + LETTERS
+    return chr(base + offset)
+
+
+## Prints a message describing proper usage.
+#
+def usage():
+    print("Usage: python cipher.py [-d] infile outfile")
+
+
+# Start the program.
+main()
---
+++ src/chapter7/total.py
@@ -0,0 +1,32 @@
+##
+# This program reads a file containing numbers and writes the numbers to
+# another file, lined up in a column and followed by their total and average.
+#
+
+# Prompt the user for the name of the input and output files.
+inputFileName = input("Input file name: ")
+outputFileName = input("Output file name: ")
+# Open the input and output files.
+infile = open(inputFileName, "r")
+outfile = open(outputFileName, "w")
+
+# Read the input and write the output.
+total = 0.0
+count = 0
+line = infile.readline()
+while line != "":
+    value = float(line)
+    outfile.write("%15.2f\n" % value)
+    total = total + value
+    count = count + 1
+    line = infile.readline()
+
+# Output the total and average.
+outfile.write("%15s\n" % "--------")
+outfile.write("Total: %8.2f\n" % total)
+avg = total / count
+outfile.write("Average: %6.2f\n" % avg)
+
+# Close the files.
+infile.close()
+outfile.close()
